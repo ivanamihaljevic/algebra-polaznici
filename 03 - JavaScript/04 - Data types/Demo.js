0f59664f3a70021d966dd45e59288abb1d244528
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-01-05 18:37:53 +0100

    Vježba

---
+++ 03 - JavaScript/04 - Data types/Demo.js
@@ -0,0 +1,9 @@
+let foo = 42; // foo is now a number
+foo = 'bar'; // foo is now a string
+foo = true; // foo is now a boolean
+const value1 = 900719925124740998n; // BigInt value
+
+// 1. Pronađite varijable u listi koje su iste po vrijednosti.
+// 2. Koja je vrijednost varijable x na kraju programa? Koji je to tip podatka?
+// 3. Koja je vrijednost varijable baaa? Objasnite.
+// 4. Provjerite tip podatka varijable w.
---
+++ 03 - JavaScript/04 - Data types/Example.js
@@ -0,0 +1,26 @@
+// Data types & Checking Data Types using typeof operator
+const name = 'Nino';
+const age = 25;
+const isWoman = false;
+const intro = 'Nino is a Front-End Lecturer at Algebra';
+const loaded = 99.9;
+const not_a_number = NaN;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isWoman);
+console.log(typeof intro);
+console.log(typeof loaded);
+console.log(typeof not_a_number);
+
+// String - Number
+// '5' becomes Number
+console.log('5' - 2);
+
+// String + Number
+// '10' stays "String", Number is added to string and becomes a string
+console.log('10' + 5)
+
+console.log(10 + - + - 10);
+
+
