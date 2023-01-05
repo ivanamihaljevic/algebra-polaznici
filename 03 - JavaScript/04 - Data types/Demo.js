09d433e41788ab9bd0595a5d01d60abe2ad59e84
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-01-05 19:21:18 +0100

    .

--- 03 - JavaScript/04 - Data types/Demo.js
+++ 03 - JavaScript/04 - Data types/Demo.js
@@ -3,7 +3,3 @@ foo = 'bar'; // foo is now a string
 foo = true; // foo is now a boolean
 const value1 = 900719925124740998n; // BigInt value
 
-// 1. Pronađite varijable u listi koje su iste po vrijednosti.
-// 2. Koja je vrijednost varijable x na kraju programa? Koji je to tip podatka?
-// 3. Koja je vrijednost varijable baaa? Objasnite.
-// 4. Provjerite tip podatka varijable w.
---
+++ 03 - JavaScript/05 - Working with text/Example.js
@@ -0,0 +1,10 @@
+const greet = 'Hello, my name is ';
+const name = 'Nino';
+
+user = greet + name;
+console.log(user)
+
+console.log('I have 25 ' + 'years');
+console.log('I have' + ' ' + '\'3\' dogs');
+
+console.log('Lorem ipsum dolor sit \namet consectetur adipiscing elit.')
