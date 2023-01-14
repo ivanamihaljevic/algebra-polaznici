e1c2a58dfb579e2acd1eb21d4b2600bc894f142c
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-01-14 11:35:53 +0100

    Update script.js

--- 03 - JavaScript/17 - Style changing/Zavrsno/script.js
+++ 03 - JavaScript/17 - Style changing/Zavrsno/script.js
@@ -3,26 +3,29 @@ var li = document.querySelectorAll("li");
 var info = document.querySelector(".info");
 var h3 = document.querySelector("h3");
 
-// 1. Zadatak
+// 1st Task
 for (var i = 0; i < 5; i++) {
     li[i].style.listStyleType = "decimal";
 }
 
 /*
-* 1. Zadatak alternativa
+* 1st Task, Tlternative Solution
 * var ul = document.querySelector("ul");
 * ul.style.listStyleType = "decimal";
 */ 
 
-// 2. Zadatak
+// 2nd Task
+// https://www.w3schools.com/jsref/prop_element_classlist.asp
 info.classList.add("hide");
 
-// 3. Zadatak
+// 3rd Task
+// https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 const bodyStyles = window.getComputedStyle(body, null);
 const margin = bodyStyles.getPropertyValue("margin-left");
 console.log(margin);
 
-// 4. Zadatak
+// 4th Task
+// https://www.w3schools.com/js/js_htmldom_animate.asp
 var marginNum = parseInt(margin);
 var animate = setInterval(frame, 50);
 var reverse = false;
