812515b5e0aeb1c8125aadf8c56a83845ff996ff
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-01-17 20:50:11 +0100

    Update scripts.js

--- 03 - JavaScript/21 - OOP JavaScript - Part 1/Završno/scripts.js
+++ 03 - JavaScript/21 - OOP JavaScript - Part 1/Završno/scripts.js
@@ -1,5 +1,28 @@
-// Ehhh, nice try. ;) 
+var character = {
+    name: "Eleven",
+    show: "Stranger Things",
+    portrayedBy: "Millie Bobby Brown",
+};
 
-// Jednom sam se zeznuo, neću 2 puta. LOL
+const quotes = [
+    "I'm going to my friends. I'm going home.",
+    "Pure fuel! PURE FUEL! WOO!",
+    "See? Zoomer.",
+    "Bitchin.",
+];
 
-// consolele(Nino)
+// 2.
+var proto1 = Object.getPrototypeOf(character);
+var proto2 = Object.getPrototypeOf(proto1);
+
+console.log(proto1);
+console.log(proto2);
+
+// 3.
+function getQuote() {
+    var index = Math.floor(Math.random() * quotes.length);
+    return quotes[index];
+}
+proto1.getQuote = getQuote;
+
+console.log(character.getQuote());
