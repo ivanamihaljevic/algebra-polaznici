55f0595fe8acd3bf66f43fbaf6325cdd4b52e780
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-02-02 19:41:45 +0100

    .

--- 03 - JavaScript/29 - TODO app/sw.js
+++ 03 - JavaScript/29 - TODO app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `todo-app-acme`;
+const CACHE_NAME = `todo-app-skuflic`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
