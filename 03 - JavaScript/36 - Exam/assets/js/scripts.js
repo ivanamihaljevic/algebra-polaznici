d1aff3a7a09b680ab506ed8f4e12846412735ba1
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-02-06 17:30:58 +0100

    Exam

---
+++ 03 - JavaScript/36 - Exam/assets/js/scripts.js
@@ -0,0 +1,8 @@
+/* Write your solutions here to get the data from https://covid19api.com (find the API URL on that page - connect to the 'summary' endpoint.)
+*  Use this text in order to save time: 
+*   
+*  COVID-19: Worldwide Statistics
+*
+*  There is a total of XXX confirmed COVID-19 cases worldwide. So far, XXX people have died because of COVID-19. 
+*  Data is updated automatically once a day. Data updated on XXX. NOTE: (Replace XXX with values from API).
+*/
