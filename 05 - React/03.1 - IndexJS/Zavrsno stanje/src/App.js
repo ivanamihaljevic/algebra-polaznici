ef864fb40a872803a7fee3ba35057f12962bc8a1
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-02-20 20:27:17 +0100

    .

---
+++ 05 - React/03.1 - IndexJS/Zavrsno stanje/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  Komponenta1,
+  Komponenta2,
+  Komponenta3,
+  Komponenta4
+} from "./Components/";
+
+export default function App() {
+  return (
+    <div>
+      <Komponenta1 />
+      <Komponenta2 />
+      <Komponenta3 />
+      <Komponenta4 />
+    </div>
+  );
+}
