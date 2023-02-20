deec1b05fd0a5da6ca717a7761627ac3c7d2c352
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-02-20 19:26:14 +0100

    .

---
+++ 05 - React/03 - IndexJS/Example/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Student from './Components/Student';
+
+
+function App() {
+  return (
+    <div>
+      <Student />
+    </div>
+  );
+}
+
+export default App;
---
+++ 05 - React/03 - IndexJS/Example/src/Components/Program.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Program() {
+    return <h2>Major: Web & Mobile Computing</h2>;
+}
