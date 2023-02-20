bfca3027fcc59758e22d54f30cd7bd2a937e7399
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-02-20 18:50:20 +0100

    .

---
+++ 05 - React/02 - Dependency Management/Završno stanje/my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import randomstring from 'randomstring';
+import moment from 'moment';
+
+function App() {
+  return (
+    <div className="App">
+      {randomstring.generate()}
+      <br/>
+      {moment().format("MMMM Do YYYY, h:mm:ss a")}
+    </div>
+  );
+}
+
+export default App;
