98a7d6b12fcacf32832d6df17c686a9e5bdf25a8
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-02-22 20:26:38 +0100

    .

--- 05 - React/06 - Components/Zavrsno stanje - korak 1/src/App.js
+++ 05 - React/06 - Components/Zavrsno stanje - korak 1/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
+// Funkcionalna komponenta
 function WelcomeFunkcion() {
   return <h1>Welcome! Komponenta definirana funkcijom.</h1>;
 }
 
+// Klasna komponenta
 class WelcomeClass extends React.Component {
   render() {
     return <h2>Welcome! Komponenta definirana klasom.</h2>;
   }
 }
 
+// Funkcionalna komponenta APP
 export default function App() {
   return (
     <div>
