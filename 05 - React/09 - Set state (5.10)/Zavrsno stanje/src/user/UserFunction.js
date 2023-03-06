a143b9537be6685730d9b334fae957d5c7baf80c
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-03-06 19:54:57 +0100

    Upload

---
+++ 05 - React/09 - Set state (5.10)/Zavrsno stanje/src/user/UserFunction.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UserFunction({ name, years }) {
+  return (
+    <p>
+      Pozdrav, moje ime je {name} i imam {years} godina.
+    </p>
+  );
+}
