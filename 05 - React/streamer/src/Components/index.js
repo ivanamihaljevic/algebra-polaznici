91396bb5a5b8beec32609c62f5f63ce1b2f42449
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-03-24 21:04:19 +0100

    .

--- 05 - React/streamer/src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as TrendingList } from './Trending/TrendingList';
-export { default as CustomNavbar } from './Navbar/CustomNavbar';
-export { default as Trending } from './Trending/Trending';
-export { default as Footer } from './Footer/Footer';
-export { default as Movies } from './Movies/Movies';
--- 05 - React/streamer/src/Helpers/utils.js
+++
@@ -1,2 +0,0 @@
-export const dateFormatting_YMD = { year: 'numeric', month: 'long', day: 'numeric' };
-export const dateFormatting_Y = { year: 'numeric' };
